622217fc69cb1ac9542a1fcfdda3c1f1e1d72e4d
RusteenZzZ <aggreex@gmail.com>
2022-01-23 13:08:37 +0400

    ert

--- src/main/java/com/example/demo/mvc/mvc.java
+++ src/main/java/com/example/demo/mvc/mvc.java
@@ -3,11 +3,12 @@ package com.example.demo.mvc;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class mvc extends WebMvcConfigurerAdapter {
+public class mvc implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT", "PATCH")
@@ -16,4 +17,6 @@ public class mvc extends WebMvcConfigurerAdapter {
                 .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
                 .allowCredentials(true).maxAge(3600);
     }
+
+
 }
